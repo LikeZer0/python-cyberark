90661659426643c5a64f4b8e90d00640458b2e68
c0pyf0x <Erik.bonizzoni@gmail.com>
2018-12-19 16:54:06 +0100

    Updated information

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(name="cyberark",
-  version="0.2",
-  description="CyberArk API functions",
-  url="https://github.com/iamtrump/python-cyberark",
-  author="Mikhail Mironov",
-  author_email="mikhailmironov@mikhailmironov.ru",
+  version="0.1.1",
+  description="CyberArk API functions without certificate validation",
+  url="https://github.com/LikeZer0/python-cyberark",
+  author="Erik Bonizzoni",
+  author_email="erik.bonizzoni@gmail.com",
   license="MIT",
   packages=["cyberark"],
   install_requires=["requests"],
